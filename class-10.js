b7db573a23c93e822576b8924bc3ed8a4b73a1cf
masum-diu <mmhmasum98@gmail.com>
2025-05-16 13:33:09 +0300

    javacript primitive

---
+++ class-10.js
@@ -0,0 +1,29 @@
+console.log("Hello World");
+// javascript 2 parts
+// primitive data types
+// string, number,boolean,null,undefinded
+
+// var,let,const
+
+var name = "sakib";
+var age = 23;
+// var isStudent = true;
+var isStudent = null;
+// var isStudent = undefined;
+// console.log(typeof name, typeof age, typeof isStudent);
+var data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(Array.isArray(data));
+console.log(typeof data[0]);
+
+var person = {
+  name: "sakib",
+  age: 23,
+  deg: "intern",
+};
+// console.log(typeof person);
+if (null === null) {
+  console.log("true");
+} else {
+  console.log("false");
+}
